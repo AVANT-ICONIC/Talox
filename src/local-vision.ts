a9164820f15ec3bb888e79cac352584dfcf32a17
Marc Ettl <87659601+AVANT-ICONIC@users.noreply.github.com>
2026-08-25 00:04:20 +0200

    feat: export local vision provider

---
+++ src/local-vision.ts
@@ -0,0 +1,7 @@
+export { createLocalVisionReasoner } from "./core/LocalVisionReasoner.js";
+export type {
+	LocalVisionConfig,
+	LocalVisionProvider,
+	OllamaVisionConfig,
+	OpenAICompatibleLocalVisionConfig,
+} from "./core/LocalVisionReasoner.js";
